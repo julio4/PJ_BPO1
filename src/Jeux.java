6854be94a298da8fae42733b2cdefd69b5c686ac
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2020-02-28 17:30:26 +0100

    Update Jeux.java

--- src/Jeux.java
+++ src/Jeux.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jeux {
 	
 	/**
@@ -5,14 +7,61 @@ public class Jeux {
 	 */
     public static void main(String[] args) {
     	Mur mur = new Mur();
+    	boolean partie = true;
     	
-    	System.out.println(mur);
+    	// MAIN NON ORDONNEE
     	
     	JeuDeCartes pile = new JeuDeCartes();
     	JeuDeCarreaux jdc = new JeuDeCarreaux();
     	
-    	Type carte = pile.tirer();
-    	System.out.println(carte);
-    	System.out.println(jdc.toString(carte));
+    	while(partie) {
+	    	System.out.println(mur);
+	    	Type carte = pile.tirer();
+	    	System.out.println(carte);
+	    	System.out.println(jdc.toString(carte));
+
+	    	//CHECK SI CARREAU POSABLE POSSIBLE
+	    	//SINON PASSE AU TOUR SUIVANT AVEC CONTINUE;
+	    	
+	    	{
+    		Scanner sc = new Scanner(System.in);
+    		//String input = sc.nextLine();
+    		//System.out.println(input);
+    		while(true) {
+    			String input = sc.next();
+	    		if(input.equals("next")) {
+	    			break;
+	    		}
+	    		else if(input.equals("stop")) {
+	    			partie = false;
+	    			break;
+	    		}
+	    		char lettre = input.charAt(0);
+	    		
+	    		int x = 0, y = 0;
+	    		while(!sc.hasNextInt()) {
+		    		sc.hasNext();
+	    		}
+	    		x = sc.nextInt();
+	    		y = sc.nextInt();
+	    		
+	    		if(/* FAIRE ICI CONDITION DE VALIDATION, 
+	    		CAD LA LETRRE ENTREE ET LES COORDONEE VERIFIABLE 
+	    		AVEC UNE LISTE DES CARREAUX POSSIBLE FAITPLUS HAUT */ 
+	    			/*valider(lettre, x, y) && lettre dans list valide*/ true) {
+	    			
+	    			//poser(l, x, y);
+	    			//SCORE
+	    			
+	    			break;
+	    		}
+	    		sc.next();
+	    		//System.out.println(lettre + " " + x + " " + y);
+	    	}
+	    	sc.close();
+	    	}
+	    	
+    	}
+    	
     }
 }
